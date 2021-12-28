7fef32e4a121ccc133d391677280d4ed0272bf4e
Toni2424 <toniuru@gmail.com>
2021-12-28 14:12:58 +0100

    mensaje

--- src/test/listarJuegosTest.java
+++ src/test/listarJuegosTest.java
@@ -1,5 +1,39 @@
 package test;
 
-public class listarJuegosTest {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class listarJuegosTest {
+	
+	private static Logger logger;
+	
+	static {
+		try {
+			logger = LogManager.getLogger(listarJuegosTest.class);
+		} catch (Throwable e) {
+			System.out.println("Don't work");
+		}
+	}
+	
+	@BeforeEach
+	public static void inicioTest() {
+		logger.info(">>>>> Inicio test");
+	}
+	
+	@AfterEach
+	public static void finTest() {
+		logger.info(">>>>> Fin test");
+	}
+	
+	
+	/*** TEST ***/
+	@Test
+	public void fechaDebeSerInt() {
+		// Given
+		
+		// When
+		
+		// Then
+	}
+	
 }
