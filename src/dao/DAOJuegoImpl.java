4341734cef0aff1d6badded99794efc1ab5ba7a2
SGarcia96 <sgcerveradev@gmail.com>
2021-12-28 10:10:39 +0100

    add menu

--- src/dao/DAOJuegoImpl.java
+++ src/dao/DAOJuegoImpl.java
@@ -1,5 +1,23 @@
 package dao;
 
-public class DAOJuegoImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Juego;
+
+public class DAOJuegoImpl implements IDAOJuego {
+
+	List<Juego> listaJuegos = new ArrayList<>();
+
+	@Override
+	public void darDeAlta() {
+		Juego juego = new Juego();
+	}
+
+	@Override
+	public void darDeAlta(Juego juego) {
+		listaJuegos.add(juego);
+		System.out.println("se ha dado de alta el juego: " + juego);
+	}
 
 }
