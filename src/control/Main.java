9b5b553a51c9edb2d024411512de9cffc275bf5d
SGarcia96 <sgcerveradev@gmail.com>
2021-12-28 07:55:36 +0100

    add utils/EntradaTeclado(steven)

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,11 +1,7 @@
 package control;
 
 public class Main {
- public static void main(String args[]) {
-	 System.out.println("hola");
-	 System.out.println("holiwis de steven");
-	 System.out.println("Gabriel");
-	 //eva
+	public static void main(String args[]) {
 
- }
+	}
 }
