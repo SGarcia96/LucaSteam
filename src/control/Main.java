5d576d83703bdb80b86cc11c1bbe748c64a7b411
EvaMontiel <evamontielaraque@gmail.com>
2021-12-28 15:57:08 +0100

    cambiar main

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,17 +1,7 @@
 package control;
 
-<<<<<<< HEAD
-import dao.DAOJuegoImpl;
-import services.JuegoServiceImpl;
-
-public class Main {
-	public static void main(String args[]) {
-		new DAOJuegoImpl().cargarJuegos();
-		new JuegoServiceImpl().listarJuegos();
-=======
 public class Main {
 	public static void main(String args[]) {
 		new GestionJuegos().inicio();
->>>>>>> 3d1f22178056a65da6f3453cbdcccf1c06955214
 	}
 }
