62ede33526f9abd500d961ffb58a947eec375401
Gabriel <gabrielmarchante@hotmail.com>
2021-12-28 16:02:36 +0100

    listarPorGenero

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,6 +1,5 @@
 package control;
 
-<<<<<<< HEAD
 import dao.DAOJuegoImpl;
 import services.JuegoServiceImpl;
 
@@ -8,10 +7,7 @@ public class Main {
 	public static void main(String args[]) {
 		new DAOJuegoImpl().cargarJuegos();
 		new JuegoServiceImpl().listarJuegos();
-=======
-public class Main {
-	public static void main(String args[]) {
 		new GestionJuegos().inicio();
->>>>>>> 3d1f22178056a65da6f3453cbdcccf1c06955214
-	}
+
+}
 }
