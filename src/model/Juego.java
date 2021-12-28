d739a836eb5f80f4d975d06507edf4feb62b2211
SGarcia96 <sgcerveradev@gmail.com>
2021-12-28 12:49:17 +0100

    add Juego constructor

--- src/model/Juego.java
+++ src/model/Juego.java
@@ -1,8 +1,14 @@
 package model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Juego {
 	@Getter @Setter private String nombre;
 	@Getter @Setter private int fecha;
