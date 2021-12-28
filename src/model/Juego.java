0710fe68113acd880e71fc1743e8e93adbf880da
SGarcia96 <sgcerveradev@gmail.com>
2021-12-28 11:42:34 +0100

    done model/Juego

--- src/model/Juego.java
+++ src/model/Juego.java
@@ -6,4 +6,7 @@ import lombok.Setter;
 public class Juego {
 	@Getter @Setter private String nombre;
 	@Getter @Setter private int fecha;
+	@Getter @Setter private Editor editor;
+	@Getter @Setter private Genero genero;
+	@Getter @Setter private Plataforma plataforma;
 }
