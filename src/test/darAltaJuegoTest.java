f3651b29ae249fe137cfc8d089390f5ecacd1570
SGarcia96 <sgcerveradev@gmail.com>
2021-12-28 17:50:34 +0100

    test alta

--- src/test/darAltaJuegoTest.java
+++ src/test/darAltaJuegoTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,12 +12,13 @@ import org.junit.jupiter.api.Test;
 import model.Genero;
 import model.Juego;
 import model.Plataforma;
+import services.JuegoServiceImpl;
 
 public class darAltaJuegoTest {
-	
+
 	private static Logger logger;
 	private static Juego juego;
-	
+
 	static {
 		try {
 			logger = LogManager.getLogger(darAltaJuegoTest.class);
@@ -24,28 +26,47 @@ public class darAltaJuegoTest {
 			System.out.println("Don't work");
 		}
 	}
-	
+
 	@BeforeEach
 	public static void inicioTest() {
 		logger.info(">>>>> Inicio test");
 		juego = new Juego();
+		// List listjuegos...
 	}
-	
+
 	@AfterEach
 	public static void finTest() {
 		logger.info(">>>>> Fin test");
 	}
-	
-	
+
 	/*** TEST ***/
+	@Test
+	public void juegoSeCreaCorrectamente() {
+		// When
+		new JuegoServiceImpl().darDeAlta(juego);
+		// Then
+	}
+	
+	@Test
+	public void juegoNotDuplicated() {
+		// Given
+		Juego juegoRepe = new Juego();
+		juegoRepe.setNombre("mario");
+		juegoRepe.setPlataforma(Plataforma.ARCADE);
+		// When
+		juego.setNombre("nombre");
+		juego.setPlataforma(Plataforma.ARCADE);
+		// Then
+		//assertThat...
+	}
+
 	@Test
 	public void fechaDebeSerMenorA1951() {
 		// Given
-		
+
 		// When
 		juego.setFecha(1500);
 		// Then
 		assertThat("fecha no valida");
 	}
-
 }
